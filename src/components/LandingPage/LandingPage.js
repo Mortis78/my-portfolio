eb2fa9daf6720705ebc3cd6d40df789a3b6bac0d
John Ammon <ammonj78@gmail.com>
2023-06-26 17:30:16 -0600

    Updated aboutMe with curent information

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,37 +5,28 @@ import './LandingPage.css'
 export const LandingPage = () => {
     return(
     <div>
-        <p>hello friends this is the LandingPage</p>
-        <div className="about-me">
-        <p>[Your Name]
-[Your Professional Title or Occupation]
+        <div className="about-me-div">
+            <p className="about-me">
+           <b>John Ammon - Softwere Developer</b>  <br/>
 
-Introduction:
-[Provide a brief and engaging introduction about yourself. Highlight your passions, expertise, or unique qualities that define you.]
+            <br/> Hey there! I'm John, a passionate front-end developer with a creative edge. I'm a proud graduate of the Turing School of Software & Design, where I honed my skills and fostered a deep love for web development.
+            
+            During my time at Turing, I had the incredible opportunity to collaborate on a multitude of exciting projects. From crafting immersive user experiences to building dynamic and responsive interfaces, I thrive in the fast-paced, collaborative environment that the school provided. These projects not only challenged my technical abilities but also taught me the value of effective teamwork and communication.
 
-Background:
-[Share some relevant details about your background, such as education, professional experience, or any notable achievements that shaped your journey.]
+            As a front-end developer, I specialize in bringing designs to life with clean, elegant code and intuitive user interactions. I have a strong command of <em>HTML, CSS, and JavaScript</em>. 
+            Currently, I'm also diving into the world of <em>TypeScript</em>, expanding my skill set and exploring the advantages it brings to modern web development."
 
-Professional Expertise:
-[Describe your areas of expertise, skills, or industries you have worked in. Highlight your key accomplishments and how they demonstrate your capabilities.]
+            Beyond code, I find inspiration in the art of web design, creating visually appealing and intuitive experiences that leave a lasting impact. I believe in the power of a well-crafted user interface to enhance journeys and spark joy. Passionate about leveraging technology for meaningful solutions, I prioritize building accessible websites and optimizing performance. My dedication ensures high-quality work that exceeds expectations.
+            
+            As a proud single parent, I have successfully raised my son for eight years, instilling in me a strong sense of responsibility, resilience, and adaptability. Together, we embrace the beauty of the mountains and find joy in every step we take.
+            <br/>
 
-Passions and Interests:
-[Talk about your personal interests and passions that drive you outside of your professional life. It could be hobbies, causes you care about, or activities that bring you joy.]
-
-Approach to Work:
-[Describe your work philosophy and how you approach your professional endeavors. Discuss any core values or principles that guide you in achieving success.]
-
-Personal Traits:
-[Highlight some of your key personality traits or qualities that make you unique. Discuss how these traits contribute to your professional and personal relationships.]
-
-Future Aspirations:
-[Share your long-term goals or aspirations, both personally and professionally. Discuss what motivates you and how you envision your future.]
-
-Conclusion:
-[Wrap up your "About Me" section by summarizing your overall message or leaving a lasting impression. You can include contact information or links to your professional profiles if desired.]
-
-Remember, this template is just a starting point. Feel free to modify it and add personal details to make it truly reflect your individuality and achievements.</p>
+            <br/>I am excited to bring my expertise, creativity, and life experiences to collaborate on boundary-pushing projects. Through technology, I aspire to make a positive impact on the world we live in. Let's create remarkable digital experiences together!"
+            </p>
         </div>
+       
+
+
     </div>
     )
 }
