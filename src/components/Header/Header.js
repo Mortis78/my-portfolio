52295690d9caf064bd1eec8b4aea168cf6f3884b
John Ammon <ammonj78@gmail.com>
2023-06-26 20:00:57 -0600

    Added Live links for Linkedin, GitHub, and Instagram

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,14 @@ export const Header = () => {
         <div>
             <h1>John Ammon - Software Engineer </h1>
             <p>Denver, CO - ammonj78@gmail.com - 720-312-3776 </p>
+            <em>
+
+            <a href="https://www.linkedin.com/in/johnathon-ammon-turing-01a86919a/" target="_blank">LinkedIn    </a> 
+
+            <a href="https://github.com/Mortis78" target="_blank"> / GitHub</a>
+            
+            <a href="https://instagram.com/mistermortis78?igshid=ZDc4ODBmNjlmNQ==" target="_blank"> / Instagram</a>
+            </em>
         </div>
     )
 }
